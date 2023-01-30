50a5d7da810bda5c0ec3e4434901bf5637b7e07a
Martin Pilaszek <martinpilaszek@gmail.com>
2023-01-30 20:11:17 -0300

    Integrated reviews list

--- components/Button.tsx
+++ components/Button.tsx
@@ -2,22 +2,30 @@ interface Props {
   secondary?: boolean,
   children?: React.ReactNode
   className?: string
-  type?: 'button' | 'submit' | 'reset' | undefined
+  type?: 'button' | 'submit' | 'reset' | undefined,
+  loading?: boolean,
+  onClick?: () => void,
 }
 
-export default function Button ({ secondary, children, className, type }: Props) {
+export default function Button ({ secondary, children, className, type, loading, onClick }: Props) {
+  const loadingStyles : string = 'opacity-50 cursor-not-allowed'
   const buttonPrimaryStyles : string = `
     rounded-md bg-sky-600 w-full py-2 border-b border-l border-r border-sky-500
     text-white font-bold shadow-md shadow-sky-900 hover:translate-y-0.5
-    ease-in-out duration-300 ${className}
+    ease-in-out duration-300 ${className} ${loading && loadingStyles}
   `
   const buttonSecondaryStyles : string = `
   rounded-md bg-slate-100 w-full py-2 border-b border-l border-r border-sky-900
   text-black font-bold shadow-md shadow-sky-900 hover:translate-y-0.5
-  ease-in-out duration-300 ${className}
-`
+  ease-in-out duration-300 ${className} ${loading && loadingStyles}
+  `
+
+  const handleClick = () => {
+    if (!loading && onClick) onClick()
+  }
+
   return (
-    <button type={type} className={secondary ? buttonSecondaryStyles : buttonPrimaryStyles}>
+    <button type={type} onClick={handleClick} className={secondary ? buttonSecondaryStyles : buttonPrimaryStyles}>
       {children}
     </button>
   )
--- components/movie/ReviewsList.tsx
+++ components/movie/ReviewsList.tsx
@@ -1,6 +1,9 @@
 import { IPaginatedList } from '@/interfaces/Paginated'
 import { IReview } from '@/interfaces/Reviews'
-import { useState } from 'react'
+import { getReviews } from '@/services/reviews'
+import { StatusCodes } from 'http-status-codes'
+import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
 import Button from '../Button'
 import Review from './Review'
 
@@ -9,18 +12,35 @@ interface Props {
 }
 
 export default function ReviewsList ({ paginatedReviews }: Props) {
+  const router = useRouter()
+  const [pageNumber, setPageNumber] = useState<number>(0)
   const [reviews, setReviews] = useState<IReview[]>(paginatedReviews.data)
   const [loading, setLoading] = useState<boolean>(false)
-  const showMore = reviews.length < paginatedReviews.totalCount
+
+  const incrementPageNumber = () => setPageNumber(pNumber => pNumber + 1)
+
+  const loadMore = async () => {
+    if (pageNumber === 0) return
+    setLoading(true)
+    const response = await getReviews({ pageNumber, pageSize: 5, movieSlug: router.query.slug as string })
+      .catch(e => e.response)
+    if (response.status === StatusCodes.OK) {
+      const data : IPaginatedList<IReview> = response.data
+      setReviews(reviews => [...reviews, ...data.data])
+    }
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    loadMore()
+  }, [pageNumber])
 
   return (
     <section className='flex flex-col items-center w-full'>
-      {
-        reviews.map((review: IReview, index: number) =>
-          <Review className={index < reviews.length ? 'mb-2' : ''} review={review} key={review.id} />
-        )
-      }
-      {showMore && <Button className='mt-2 max-w-xs'>Load more</Button>}
+      {reviews.map((review: IReview, index: number) =>
+        <Review className={index < reviews.length ? 'mb-2' : ''} review={review} key={review.id} />
+      )}
+      {reviews.length < paginatedReviews.totalCount && <Button onClick={incrementPageNumber} loading={loading} className='mt-2 max-w-xs'>Load more</Button>}
     </section>
   )
 }
