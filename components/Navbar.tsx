264aaf2c65187d0e13e76126798d6e435dbbf8f3
Martin Pilaszek <martinpilaszek@gmail.com>
2023-02-01 17:19:07 -0300

    Added responsive navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,12 +9,12 @@ export default function Navbar () {
   return (
     <header className='h-16 mb-5 flex flex-row items-center bg-gray-800 sticky top-0'>
       <Container>
-        <nav className='w-full flex flex-row items-center'>
-          <Link className='inline-block mr-auto' href='/'>
+        <nav className='w-full flex justify-between flex-row gap-4 items-center'>
+          <Link className='inline-block w-16' href='/'>
             <img src='/logo.png' className='w-16' />
           </Link>
-          <input className='flex h-8 rounded-full w-96 self-center px-3 py-2' placeholder='Search' value={search} onChange={changeSearch} />
-          <Link href='/' className='inline-block ml-auto'>
+          <input className='flex h-8 rounded-full w-42 md:w-64 px-3 py-2' placeholder='Search' value={search} onChange={changeSearch} />
+          <Link href='/' className='inline-block w-6'>
             <img className='w-6 h-6 rounded-full' src='/avatar_placeholder.png' alt='User avatar' />
           </Link>
         </nav>
